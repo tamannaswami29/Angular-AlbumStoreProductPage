f98bc4a7b7f46307e0516c57d49bb9986716c2b1
Tamanna Swami <tswami@xpanxion.com>
2019-02-06 16:13:28 +0530

    import observable in description class

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http,Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Album} from './album';
-import {Observable} from 'rxjs/observable';
+import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class ProductService {
 private _albumUrl='../assets/album.json';
